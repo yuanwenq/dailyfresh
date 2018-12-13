d16484ec8eefca44d0883797901b513f1f92fdda
YW <hyw232@163.com>
2018-12-13 16:54:49 +0800

    完成商品评价功能

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from order.views import OrderPlaceView, OrderCommitView, OrderPayView
+from order.views import OrderPlaceView, OrderCommitView, OrderPayView, CheckPayView, CommentView
+
 urlpatterns = [
     url(r'^place$', OrderPlaceView.as_view(), name='place'), # 提交订单页面显示
     url(r'^commit$', OrderCommitView.as_view(), name='commit'), # 提交创建
     url(r'^pay$', OrderPayView.as_view(), name='pay'), # 订单支付
+    url(r'^check$', CheckPayView.as_view(), name='check'), # 查询支付订单结果
+    url(r'^comment/(?P<order_id>.+)$', CommentView.as_view(), name='comment'), # 订单评论
 ]
